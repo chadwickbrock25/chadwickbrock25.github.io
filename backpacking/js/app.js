8bdd0ae4e8120f1ac71a9f0d3b2533188c5861d4
Chadwick Mitchell Brock <Chadwick@Chadwicks-Pro.attlocal.net>
2020-06-16 20:09:38 -0400

    Tested weather apikey & waiting for activation

--- backpacking/js/app.js
+++ backpacking/js/app.js
@@ -1,1 +1,10 @@
 console.log("GOod luck")
+
+const baseURL = "http://history.openweathermap.org/data/2.5/history/                accumulated_temperature?"
+
+const apiKey = "apikey=ab680529730b171d229d833b949afad3"
+const queryType = "q="
+let cityQuery = "atlanta, US"
+let queryURL = "http://history.openweathermap.org/data/2.5/history/accumulated_temperature?q={atlanta,USA}&start={1587805563}&end={1587805563}&threshold={284}&appid={ab680529730b171d229d833b949afad3}"
+
+console.log(queryURL)
